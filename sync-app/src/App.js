174c40f2bee6dc7d16adfb898c60076770041864
ZahinSam1 <toky.jahin@gmail.com>
2023-06-07 17:14:23 +0600

    tested for backend and ddatabase

---
+++ sync-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Fetch_API from "./components/fetch_api";
+import TestForm from "./components/form";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is App component</h1>
+      {/* <Fetch_API /> */}
+      <TestForm />
+    </div>
+  );
+};
+
+export default App;
