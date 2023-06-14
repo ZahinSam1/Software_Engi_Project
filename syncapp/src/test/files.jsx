05daa131735fae5141f1dc996272b530d48080cb
ZahinSam1 <toky.jahin@gmail.com>
2023-06-14 15:20:48 +0600

    update

--- syncapp/src/test/files.jsx
+++ syncapp/src/test/files.jsx
@@ -1,20 +1,53 @@
-import { AiOutlineFile } from "react-icons/ai";
+// import { FcFolder, FcFile } from "react-icons/fc";
+import "./files.css";
+import FileStruct from "./fileStructure";
 
-const FileComponent = () => {
-    return ( <div>
-        <div>
-          <AiOutlineFile />
-          <span>File 1</span>
-        </div>
-        <div>
-          <AiOutlineFile />
-          <span>File 2</span>
-        </div>
-        <div>
-          <AiOutlineFile />
-          <span>File 3</span>
-        </div>
-      </div> );
-}
- 
+const FileComponent = ({ folderName, folderContent }) => {
+  // if (folderName === undefined) {
+  //   folderName = "Folder Name";
+  // }
+  const folderData = {
+    name: "Folder 1",
+    type: "folder",
+    children: [
+      {
+        name: "Subfolder 1",
+        type: "folder",
+        children: [
+          {
+            name: "File 1",
+            type: "file",
+          },
+          {
+            name: "File 2",
+            type: "file",
+          },
+        ],
+      },
+      {
+        name: "Subfolder 2",
+        type: "folder",
+        children: [
+          {
+            name: "File 3",
+            type: "file",
+          },
+        ],
+      },
+      {
+        name: "File 4",
+        type: "file",
+      },
+    ],
+  };
+
+  return (
+    // <div className="FolderIconContainer">
+    //   <FcFolder className="FolderIcon" />
+    //   <span className="FolderName">{folderName}</span>
+    // </div>
+    <FileStruct data={folderData} />
+  );
+};
+
 export default FileComponent;
